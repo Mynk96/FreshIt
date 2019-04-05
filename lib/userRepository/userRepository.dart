de92aa3b192c8246542c71de3ea7ef79dd55062b
Mynk96 <mayank.harsani@gmail.com>
2019-04-05 17:06:43 +0530

    added firebase_auth for signIn

--- lib/userRepository/userRepository.dart
+++ lib/userRepository/userRepository.dart
@@ -1,14 +1,23 @@
 import 'package:meta/meta.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserRepository {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   Future<String> authenticate({
     @required String username,
     @required String password,
   }) async {
-    await Future.delayed(Duration(seconds: 3));
-    if (username == "mayank.harsani@gmail.com" && password == "12345")
-      return 'token';
-    throw Exception("Ïnvalid Login details");
+    try {
+      FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
+          email: username, password: password);
+      return user.getIdToken();
+    } catch (error) {
+      throw error.message;
+    }
+    // await Future.delayed(Duration(seconds: 3));
+    // if (username == "mayank.harsani@gmail.com" && password == "12345")
+    //   return 'token';
+    // throw Exception("Ïnvalid Login details");
   }
 
   Future<void> deleteToken() async {
